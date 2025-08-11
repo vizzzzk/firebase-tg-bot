b33c5184b9e7a0c1af23987e37af381279ad78dd
Incoming Virus <Incomingvirus.IV@gmail.com>
2025-08-11 08:34:15 +0000

    Set up Firebase Hosting Install Firebase CLI
    Initialize your project

---
+++ src/lib/firebase.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCNVzTY9CXY7J_qWPxivPK-EkZgKUcK6Ic",
+  authDomain: "webot-scm5f.firebaseapp.com",
+  projectId: "webot-scm5f",
+  storageBucket: "webot-scm5f.appspot.com",
+  messagingSenderId: "634655886203",
+  appId: "1:634655886203:web:3313a4d57bce49d8ec8bde"
+};
+
+// Initialize Firebase
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
+export { app };
